1944889f4e5c81d9bb72a460f2d24cfd0475cea6
Yogeshwargundale <yogeshwargundale@gmail.com>
2020-02-21 09:18:39 +0530

    added docker image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React,{ Component } from 'react';
-// import './style.css';
+import './style.css';
 import {Card ,Row, Col,Switch, Icon} from 'antd';
 const axios = require('axios');
 const plan='monthly';
@@ -9,59 +9,32 @@ class App extends Component {
         super(props);
         this.state={
             data :[],
-            name:'apptitle',
-            plan:''
+            plan:'monthly'
         }
     }
 
     componentDidMount(){
-      localStorage.setItem(plan,'monthly')
     this.fetchData();
-  }
-
-  call= (res)=>{
-    //   this.setState({data:res})
-      console.log(res)
   }
 
   onPlanChange = (checked)=>{
     
     console.log(checked);
-    {checked?localStorage.setItem(plan,'year'):localStorage.setItem(plan,'monthly')};
-    console.log(localStorage.getItem(plan));
+    {checked?this.setState({'plan':'year'}):this.setState({plan:'monthly'})}
     this.fetchData();
 
   }
   fetchData = ()=>{
       
-        // axios.get('http://www.mocky.io/v2/5e3156863200007200888450')
-        //   .then(function (response) {
-        //     // handle success
-            
-        //     console.log(response);
-        //     const resp = response.data.plans;
-           
-        //     const result = resp.filter((item)=> item.interval ===localStorage.getItem(plan));
-            
-        //     // const year = resp.filter((item)=> item.interval ==='year')
-        //     // const monthly = resp.filter((item)=> item.interval ==='monthly')
-        //     // console.log(year);
-        //     // console.log(monthly);
-        //     console.log(result);
-            
-        //   })
-        //   .catch(function (error) {
-        //     // handle error
-        //     console.log(error);
-        //   })
+       
         var self=this;
           axios({
             method: 'get',
-            url: 'http://www.mocky.io/v2/5e4a880b2f0000290097d2d2',
+            url: 'http://www.mocky.io/v2/5e3156863200007200888450',
           })
             .then(function (response) {
               const resp = response.data.plans;
-              const result = resp.filter((item)=> item.interval ===localStorage.getItem(plan));
+              const result = resp.filter((item)=> item.interval ===self.state.plan);
               self.setState({data:result});
               console.log(self.state.data);
               
@@ -74,48 +47,51 @@ class App extends Component {
 
   render(){
     return (
-      // <div className="App">
-      // <h2>plan details </h2>
-    
-      //     <div><Switch defaultChecked onChange={this.onPlanChange} /></div> 
-      //   <Card
-      //    hoverable
-      //   >
-
-      //   </Card>
-      // </div>
       <div>
+
       <Row>
-         <Col span={4}></Col>
-        <Col span={16}> 
-        <h2>plan details : {localStorage.getItem(plan) }</h2>
-        <div>MONTHLY <Switch  onChange={this.onPlanChange} /> YEARLY</div>
-        <Row>
-        {this.state.data.map((info)=>{
-         return (
-                  <Col span={4}>
-                  <Card 
-                    title={info.planName} 
-                    cover={<Icon type='user'></Icon>}
-                    hoverable 
-                    bordered={false}
-                    style={{ width: 240 }}  
-                    >
-                      <span>{info.planDescription}</span><br/>
-                      <span>{info.currencySymbol}{info.amount}/month</span>
-                      {
-                        info.features.map((list)=>{
-                          return <ul><li>{list}</li></ul>
-                        })
-                      }
-                       
-                  </Card>
-                  </Col>
-                )
-        })}
-        </Row>
-        </Col>
-        <Col span={4}></Col>
+            {/* <Col span={1}></Col> */}
+            {/* <Col span={16}>  */}
+
+            <Row >
+              <Col offset={2} span={3}></Col>
+                <Col span={6}>
+                <div style={{padding:'25px'}}>
+                    <span className='plan-caption'>MONTHLY</span> 
+                      <Switch  onChange={this.onPlanChange} /> 
+                    <span className='plan-caption'>YEARLY</span> <br/>
+                </div>
+                </Col>
+              </Row>
+            <div >
+            <Row >
+              <Col offset={2} span={3}></Col>
+            {this.state.data.map((info)=>{
+            return (
+                      <Col gutter={1} md={4} span={4}>
+                      <Card 
+                        title={info.planName} 
+                        cover={<Icon type='user'></Icon>}
+                        hoverable 
+                        
+                        style={{ width: 240 ,borderRadius:'15px',alignContent:'center'}}  
+                        >
+                          <span className='desc'>{info.planDescription}</span><br/><br/>
+                          <div><span className='currency'>{info.currencySymbol}</span><span className='amt'>{info.amount}</span><span className='month'>/month</span></div><br/><br/>
+                          {
+                            info.features.map((list,id)=>{
+                            return <div ><Icon style={{color:'green'}}type='check' />{' '}{' '}<span key={{id}}className='features'>{list}</span></div>
+                            })
+                          }
+                          
+                      </Card>
+                      </Col>
+                    )
+            })}
+            </Row>
+        </div>
+        {/* </Col> */}
+        {/* <Col span={4}></Col> */}
       </Row>
       </div>
     )
